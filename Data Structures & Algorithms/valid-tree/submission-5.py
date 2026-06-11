712bbdccca63d19688dab549dcef049af265ea98
Rohan Sheelvant <44243145+rohansheelvant@users.noreply.github.com>
2026-06-10 23:44:40 -0500

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.py
@@ -0,0 +1,47 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+
+        if len(edges) != n-1:
+            return False
+
+        nodes = { i : [] for i in range(n)}
+
+        for n1, n2 in edges:
+            nodes[n1].append(n2)
+            nodes[n2].append(n1)
+        
+        checked_nodes = set()
+
+        def check_acyclic(node):
+            q = deque([])
+            visited = set()
+            checked_nodes.add(node)
+            q.append((node, None))
+            while(q):
+                ele, prev = q.popleft()
+                if ele in visited:
+                    return False
+                visited.add(ele)
+                checked_nodes.add(ele)
+                neighbors = nodes[ele]
+                for neigh in neighbors:
+                    if neigh == prev:
+                        continue
+                    q.append((neigh, ele))
+            
+            return True
+        
+        for node in range(n):
+            if node in checked_nodes:
+                continue
+            if not check_acyclic(node):
+                return False
+        
+        return True
+
+
+        
+        
+
+        
+        

4a3d859821c66c687a6e7f9aad2ef6c9fa0a9ad6
Rohan Sheelvant <44243145+rohansheelvant@users.noreply.github.com>
2026-06-17 18:40:48 -0500

    Add: hand-of-straights - submission-3

---
+++ Data Structures & Algorithms/hand-of-straights/submission-3.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        len_hand = len(hand)
+        no_of_grps = int(len_hand//groupSize)
+
+        if len_hand % groupSize != 0:
+            return False
+        
+        item_counter = defaultdict(int)
+        for val in hand:
+            item_counter[val] += 1
+        min_val, max_val = min(hand), max(hand)
+
+        start_rem = no_of_grps
+
+        for i in range(min_val, max_val+1):
+            if start_rem != 0:
+                count = item_counter[i]
+                item_counter[i] = 0
+                start_rem -= count
+                if start_rem < 0:
+                    return False
+                for cnt in range(count):
+                    for j in range(i+1, i+groupSize):
+                        if item_counter[j] <= 0:
+                            return False
+                        item_counter[j] -= 1
+        if start_rem != 0:
+            return False
+        
+        return True
+
+
+
